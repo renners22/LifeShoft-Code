6aef70da4ec3d7afaf6fc059ed140c89be80fbf6
Jonathan-Lora <77507489+Jonathan-Lora@users.noreply.github.com>
2021-01-15 14:57:12 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var getData = function(){
+    var nombre = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    console.log("tu nombre es, " + nombre + " y tu edad es " + age);
+}
+
+console.log(getData);
